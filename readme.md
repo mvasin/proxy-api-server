a031eea2097de7ddaf5a04b76e19e1f972f68545
Mikhail Vasin <mvasin@users.noreply.github.com>
2019-05-29 22:48:04 +0300

    Add static server for /app folder and proxy /api to giphy.com

--- readme.md
+++ readme.md
@@ -1,9 +1,10 @@
-# GIPHY proxy API server
-Injects GIPHY API key into request to GIPHY endpoint.
+# A static web server with API proxy that keeps API keys confidential
+This web server serves static assets and adds API key to every upstream API request. So your API key is safe and invisible.
 
-This is a bare bones Node.js server, written without using any frameworks.
+## Currently a GIPHY proxy API server
+It could be done more generic, but currently it's tailored for giphy.com API.
+Put your giphy.com API key to `API_KEY` environment variable and enjoy hiding it from outer world. The proxy server will add `api_key=xxx` to your URL search string in requests to upstream API.
 
-## How to use
-1. Put your key into GIPHY_API_KEY environment variable and start the proxy server.
-2. Build with `./node_modules/.bin/tsc`
-3. Run with `yarn prod`
+It currently serves only `GET` requests. All `/v1/gifs/search` requests are routed to `https://api.giphy.com/v1/gifs/search`, for the rest it tries to serve `/app` directory.
+
+(c) MIT Mikhail Vasin
--- src/app.ts
+++ src/app.ts
@@ -1,49 +1,72 @@
-import http, {RequestListener} from 'http';
+
+
+import http, {RequestListener, RequestOptions} from 'http';
 import https from 'https';
+import serveHandler from 'serve-handler';
 
-const port = 4000
 
-const GIPHY_API_KEY = process.env.GIPHY_API_KEY;
-
-console.log('hey')
-
-if (!GIPHY_API_KEY) {
-  throw Error('GIPHY_API_KEY environment variable is not set');
+const PROXY_PORT = 8080;
+const {API_KEY} = process.env;
+if (!API_KEY) {
+  throw Error('API_KEY environment variable is not set!');
 }
 
-const requestListener: RequestListener = (request, response) => {
-  const params = getParams(request);
-  params.set('api_key', GIPHY_API_KEY);
-  const giphy_url = `https://api.giphy.com/v1/gifs/search?${params}`;
-  https.get(giphy_url, (resp) => {
-    let data = '';
+const requestListener: RequestListener = (proxyRequest, proxyResponse) => {
+  try {
+    if (proxyRequest.method !== 'GET') {
+      proxyResponse.statusCode = 405;
+      proxyResponse.setHeader('Content-Type', 'application/json');
+      proxyResponse.end(JSON.stringify({error: "Method not allowed"}))
+      return;
+    }
 
-    // TBD: incoming stream should be routed directly to the outgoing stream
-    resp.on('data', (chunk) => {
-      data += chunk;
-    });
-  
-    // The whole response has been received. Print out the result.
-    resp.on('end', () => {
-      response.end(data)});
-  
-  }).on("error", err => {throw err});
+    if (proxyRequest.url && proxyRequest.url.startsWith('/api')) {
+      const upstreamOptions = getUpstreamOptions(proxyRequest);
+      const connector = https.request(upstreamOptions, upstreamResponse => upstreamResponse.pipe(proxyResponse))
+      proxyRequest.pipe(connector);
+      return;
+    }
+
+    // static files from /app folder
+    serveHandler(proxyRequest, proxyResponse, {
+      public: '/app',
+      cleanUrls: true
+    })
+  } catch(error) {
+    console.error(error);
+    proxyResponse.statusCode = 404;
+    proxyResponse.setHeader('Content-Type', 'application/json');
+    proxyResponse.end(JSON.stringify({error: "Server error"}))
+  }
 }
 
-const server = http.createServer(requestListener)
-
-server.on('error', e => {
-  console.log(e);
-});
-
-server.listen(port, () => {
-    console.log(`server is listening on ${port}`)
+http.createServer(requestListener).listen(PROXY_PORT, () => {
+    console.log(`Started to listen on port ${PROXY_PORT}`);
 })
 
-function getParams(request: http.IncomingMessage): URLSearchParams {
+/**
+ * Generate response similar to incoming request, but routes to upstream
+ * server and sets `apy_key=${API_KEY}` query param.
+ */
+function getUpstreamOptions(request: http.IncomingMessage): RequestOptions {
   if (!request.url) throw Error('Invalid URL');
   const arr = request.url.split('?');
   arr.shift();
   const str = arr.join('');
-  return new URLSearchParams(str);
+  const params = new URLSearchParams(str);
+  params.set('api_key', API_KEY as string);
+
+  const pathWithQuery = ['/v1/gifs/search', params].join('?')
+  const upstreamOptions = {
+    protocol: 'https:',
+    host: 'api.giphy.com',
+    family: 4,
+    port: 443,
+    method: 'GET',
+    path: pathWithQuery,
+    headers: {Accept: "application/json"},
+    timeout: 1000
+  };
+  console.log(upstreamOptions);
+  return upstreamOptions;
 }
